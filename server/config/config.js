b159597ea735ea9530b84a74f2372efd5b82ef84
jcaceitunowebimpacto <jcaceituno@webimpacto.es>
2019-05-09 13:01:40 +0200

    Variable de entorno MONGO añadida

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if(process.env.NODE_ENV==='dev'){
     urlDB='mongodb://localhost:27017/cafe';
 }else{
-    urlDB='mongodb+srv://cafe-user:hxma5JahvAxM8Dze@cluster0-qwcgl.mongodb.net/cafe';
+    urlDB=process.env.MONGO_URI;
 }
 
 process.env.URLDB=urlDB;
